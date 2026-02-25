d7c3e2b4cb9e4339cdc8a10be7b8ba78e4ce6487
Isaac Sharpe <iesharpe05@gmail.com>
2026-02-25 14:30:10 +0000

    restructured the repo.

---
+++ api/home.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+require_once dirname(path: __DIR__) . "/src/Config.php";
+
+$user = $tokenAuth->AuthenticateJWTToken();
+
+$bikes = $bikeGateway->getByUserId(id: $user['user_id']);
+
+
+?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gloucester Bike Registry</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    
+    <?php include_once dirname(path: __DIR__) . '/php/Navbar.php'; ?>
+
+</body>
+
+</html>
