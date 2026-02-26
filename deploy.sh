671cc58863d666235389feef83a45906444433e8
Isaac Sharpe <iesharpe05@gmail.com>
2026-02-26 14:59:34 +0000

    Added a prompt to the delploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ source .env
 npm run build
 
 mkdir build/public
-mv build/* build/public/
+mv build/* build/public/ 2> /dev/null #Silently fails to move /public into itself.
 mv build/.htaccess build/public/
 
 cp -r public/api build/public/
@@ -17,4 +17,13 @@ mkdir -p build/src/exceptions
 cp src/*.php src/.htaccess build/src/
 cp src/exceptions/* build/src/exceptions/
 
-rsync -r -e "ssh -i $SSH_KEY_PATH" "build/" "$SERVER_USER@$SERVER_ADDRESS:$SERVER_PATH"
+
+read -q "push?Push new build to the remote server? [y/N] "
+
+if [[ $push =~ ^[yY]$ ]]
+then
+    rsync -v --stats --progress -r -e "ssh -i $SSH_KEY_PATH" "build/" "$SERVER_USER@$SERVER_ADDRESS:$SERVER_PATH"
+    printf "\nComplete"
+else
+    printf "\nBuild created."
+fi
